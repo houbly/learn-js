5572dd66735e3a92a0b5d9344fa411a339e5e124
Houston Blyden <h.blyden@gmail.com>
2023-09-18 21:10:30 +0100

    add button text toggle

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
+let isToggleOpen = true;
+
 $('.toggle-button').on('click', () => {
   $('.toggle-list').slideToggle();
+  isToggleOpen = !isToggleOpen;
+  if(isToggleOpen) {
+    $('.toggle-button').html('Show less');
+  } else {
+    $('.toggle-button').html('Show more');
+  }
 })
